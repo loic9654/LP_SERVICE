7fb5d1f370fdb554a4712e7822fcb761cfad792f
loic9654 <loic0gregoire@gmail.com>
2019-01-21 10:49:16 +0100

    ajout de la route part titre

---
+++ db_apis/serie_titres.js
@@ -0,0 +1,23 @@
+const database = require('../services/database.js');
+
+const baseQuery =
+ `select * from SERIE`;
+
+async function find(context) {
+  let query = baseQuery;
+  const binds = {};
+
+
+  if (context.id) {
+    binds.Titre = `\'`+ context.id + `\'`;
+
+    query += `\n where Titre = :Titre`;
+    
+  }
+
+  const result = await database.simpleExecute(query, binds);
+
+  return result.rows;
+}
+
+module.exports.find = find;
