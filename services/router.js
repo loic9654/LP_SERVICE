f77c7b991faf65122b161d104726d695882af401
loic9654 <loic0gregoire@gmail.com>
2019-01-22 10:44:25 +0100

    cors

--- services/router.js
+++ services/router.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const router = new express.Router();
 const series = require('../controllers/series.js');
 const serie_titres = require('../controllers/serie_titres.js');
+
+express.use(cors());
 
 router.route('/series/')
   .get(series.get);
