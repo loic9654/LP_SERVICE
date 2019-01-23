37a85110a81cf224d375d5b76df70dee4f0a37a4
loic9654 <you@example.com>
2019-01-23 13:21:25 -0500

    cors solve \o/

--- services/web-server.js
+++ services/web-server.js
@@ -19,12 +19,16 @@ function initialize() {
 
     app.use((req,res,next) => {
       res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Headers', 'origin,X-Requested-With,Content-Type,accept, Authorization');
+      res.header('Access-Control-Allow-Headers', '*');
+      
+/*
+      if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', '*');
+        return res.status(200).json();
+      }*/
+      next();
     });
-    if(req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE,GET');
-      return res.status(200).json();
-    }
+    
 
     app.use('/api', router);
 
