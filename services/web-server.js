e321977f22805c78f127149fa459fb8e7255ab36
loic9654 <loic0gregoire@gmail.com>
2019-03-17 16:54:55 +0100

    debug findUSer

--- services/web-server.js
+++ services/web-server.js
@@ -20,15 +20,16 @@ function initialize() {
     app.use((req,res,next) => {
       res.header('Access-Control-Allow-Origin', '*');
       res.header('Access-Control-Allow-Headers', '*');
-      
-/*
+
+
       if(req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', '*');
         return res.status(200).json();
-      }*/
+        console.log("option in header");
+      }
       next();
     });
-    
+
 
     app.use('/api', router);
 
