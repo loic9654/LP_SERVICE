883e0a3daadeae71dc90126b1e1f95b895e30462
loic9654 <loic0gregoire@gmail.com>
2019-05-08 16:13:34 +0200

    modify password untested

--- controllers/all_user.js
+++ controllers/all_user.js
@@ -43,14 +43,11 @@ async function post(req, res, next) {
 async function modifyPass(req, res, next) {
   console.log('password changed');
 
-  try {
     let user = getUseFromReq(req);
 
     user = await users.modifyPass(user);
     res.status(201).json(user);
-  } catch (err) {
-    next(err);
-  }
+
 }
 
 module.exports.post = modifyPass;
