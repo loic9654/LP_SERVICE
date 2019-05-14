0fb2bf6be97394e6c39b6e038d1984ed7aa17b6f
loic9654 <loic0gregoire@gmail.com>
2019-05-14 19:43:09 +0200

    debug

--- services/database.js
+++ services/database.js
@@ -1,34 +1,34 @@
 const oracledb = require('oracledb');
 const dbConfig = require('../config/database.js');
- 
+
 async function initialize() {
   const pool = await oracledb.createPool(dbConfig.hrPool);
 }
- 
+
 module.exports.initialize = initialize;
 
 async function close() {
   await oracledb.getPool().close();
 }
- 
+
 module.exports.close = close;
 
 function simpleExecute(statement, binds = [], opts = {}) {
   return new Promise(async (resolve, reject) => {
     let conn;
- 
+
     opts.outFormat = oracledb.OBJECT;
     opts.autoCommit = true;
- 
+
     try {
       conn = await oracledb.getConnection();
- 
+
       const result = await conn.execute(statement, binds, opts);
- 
       resolve(result);
     } catch (err) {
       reject(err);
     } finally {
+      console.log(result);
       if (conn) { // conn assignment worked, need to close
         try {
           await conn.close();
@@ -39,5 +39,5 @@ function simpleExecute(statement, binds = [], opts = {}) {
     }
   });
 }
- 
+
 module.exports.simpleExecute = simpleExecute;
