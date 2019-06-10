a234ce356d9619755a045466f59e440ff68d92cc
loic9654 <loic0gregoire@gmail.com>
2019-06-10 22:15:58 +0200

    dammit again, forgot the export

--- controllers/all_user.js
+++ controllers/all_user.js
@@ -71,7 +71,7 @@ async function modifyPass(req, res, next) {
 }
 
 
-module.export.authenticate = authenticate
+module.export.authenticate = authenticate;
 module.exports.modifyPass = modifyPass;
 module.exports.post = post;
 module.exports.get = get;
