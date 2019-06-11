a4ee45bdc8a90994e4b1222bc6bb5ca9410eef92
loic9654 <loic0gregoire@gmail.com>
2019-06-11 22:19:17 +0200

    debug moy note

--- db_apis/series.js
+++ db_apis/series.js
@@ -18,7 +18,7 @@ async function find(context) {
 async function getNote(context) {
 
   serie = context.serie;
-  let query = `select * as notemoy from notation where ID_SERIE='13'`;
+  let query = `select * from notation where ID_SERIE='13'`;
   console.log(query);
   const result = await database.simpleExecute(query);
   console.log(result);
