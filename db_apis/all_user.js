130565dba5a3746ee4dd72af49884445a0fe77dc
loic9654 <loic0gregoire@gmail.com>
2019-06-12 17:16:23 +0200

    verify token

--- db_apis/all_user.js
+++ db_apis/all_user.js
@@ -35,7 +35,7 @@ async function authenticate(context) {
 function verifyToken(context) {
   var passedtok = context.token;
   for (const tok in tokenarray) {
-      console.log(tok["token"]["token"]);
+      console.log(JSON.parse(tok));
       // if (tok.token.token == passedtok){
       //   return {"success" : "True"}
       // }
